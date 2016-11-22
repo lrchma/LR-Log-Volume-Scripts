8f75cb17e44158ab8f58bc0522a53563e49c4d99
lrchma <email.cmartin@gmail.com>
2016-11-22 10:15:07 -0500

    Update OS-Windows-GetEventLogVolume.ps1

--- OS-Windows-GetEventLogVolume.ps1
+++ OS-Windows-GetEventLogVolume.ps1
@@ -6,3 +6,10 @@ $Duration = New-Timespan –Start $StartDate –End $LastEventLog
 $MPStemp =  $TotalEvents / $Duration.TotalSeconds
 $MPS = $MPStemp -replace "-"
 Write-Host "MPS :" $MPS
+
+#Variation of the above but broken down by Day, Hour or Minute
+#Thanks to Brent Branson @LR
+
+#Get-EventLog -LogName Security -After ((Get-Date).Date.AddDays(-30)) | Group-Object {$_.TimeWritten.Date}
+#Get-EventLog -LogName Security -After ((Get-Date).Date.AddDays(-1)) | Group-Object {$_.TimeWritten.Hour}
+#Get-EventLog -LogName Security -After ((Get-Date).Date.AddHours(-1)) | Group-Object {$_.TimeWritten.Minute}
